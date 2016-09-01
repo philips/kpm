2650fa2378da3c5d404833a7011255aa6ed708d2
Antoine Legrand <2t.antoine@gmail.com>
2016-09-01 07:00:00 +0200

    [Cmd] Update pull options

--- kpm/commands/pull.py
+++ kpm/commands/pull.py
@@ -16,30 +16,19 @@ class PullCmd(CommandBase):
         self.package = options.package[0]
         self.registry_host = options.registry_host
         self.version = options.version
-        self.dest = options.tmpdir
+        self.dest = options.dest
         self.path = None
         super(PullCmd, self).__init__(options)
 
     @classmethod
     def _add_arguments(self, parser):
         parser.add_argument('package', nargs=1, help="package-name")
-        parser.add_argument("--tmpdir", nargs="?", default="/tmp/",
+        parser.add_argument("--dest", nargs="?", default="/tmp/",
                             help="directory used to extract resources")
-        parser.add_argument("--dry-run", action='store_true', default=False,
-                            help="do not create the resources on kubernetes")
-        parser.add_argument("--namespace", nargs="?",
-                            help="kubernetes namespace", default=None)
-        parser.add_argument("--api-proxy", nargs="?",
-                            help="kubectl proxy url", const="http://localhost:8001")
         parser.add_argument("-v", "--version", nargs="?",
                             help="package VERSION", default=None)
         parser.add_argument("-x", "--variables",
                             help="variables", default={}, action=kpm.command.LoadVariables)
-        parser.add_argument("--shards",
-                            help="Shards list/dict/count: eg. --shards=5 ; --shards='[{\"name\": 1, \"name\": 2}]'",
-                            default=None)
-        parser.add_argument("--force", action='store_true', default=False,
-                            help="force upgrade, delete and recreate resources")
         parser.add_argument("-H", "--registry-host", nargs="?", default=kpm.registry.DEFAULT_REGISTRY,
                             help='registry API url')
 
